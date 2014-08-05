5daa348a99b55924dc9ed35d5272b0e82db0a4c7
Sophie <a.sophiewirth@gmail.com>
2014-08-05 14:36:14 +0200

    Gnarf

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,25 +6,24 @@ App.prototype.render = function() {
   // How to create an HTML element
   var headerElement = $('<h1>Hi Sophie :)</h1>');
 
-  var tableOpen = $('<table>');
+  var htmlString = '<table>';
   for(var i = 0; i < board.tilesToASide; i++) {
-    var trOpen = trOpen + $('<tr>');
+    htmlString += '<tr>';
     for (var j = 0; j < board.tilesToASide; j++) {
-      var tableData = tableData + $('<td></td>');
+      htmlString += '<td></td>';
     }
-    var trClosed = $('</tr>');
-    var tableClosed = $('</table>');
+    htmlString += '</tr>';
   }
+  htmlString += '</table>';
+
 
 
   // How to find an element on the page (using a CSS selector)
   var insertionPoint = $('#insertion-point');
 
   // How to replace the content of an element with HTML you've created
- var stuffToInsert = $('tableOpen' + 'trOpen' + 'tableData' + 'trClosed' + 'tableClosed');
-
-  insertionPoint.html(stuffToInsert);
-
+  insertionPoint.html(htmlString);
+  console.log(htmlString);
   // Your mission, should you choose to accept it, is to render the board you've been given
   // on the page. You have the building blocks you need:
   //   - You can create any HTML element by passing it as a string to the dollar function ($)
