fcb0aecbecd5e350003fddbc6751d107426b0f2c
Sophie <a.sophiewirth@gmail.com>
2014-08-08 12:29:25 +0200

    Problems with tds

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,15 +2,41 @@ App = function(board) {
   this.board = board;
 };
 
-App.prototype.render = function() {
-  // How to create an HTML element
-  var headerElement = $('<h1>Hi Sophie :)</h1>');
 
+App.prototype.render = function() {
+
+  var middle = ((Math.round(board.tilesToASide/2))-1);
   var htmlString = '<table>';
-  for(var i = 0; i < board.tilesToASide; i++) {
+  var r = 0;
+  var v = 0;
+  for(var tr = 0; tr < board.tilesToASide; tr++) {
     htmlString += '<tr>';
-    for (var j = 0; j < board.tilesToASide; j++) {
-      htmlString += '<td></td>';
+    for (var td = 0; td < board.tilesToASide; td++) {
+      if((tr == middle)&&(td == middle)){
+        htmlString += '<td id="middle"></td>';
+      }
+      else if(tr == 0) {
+        htmlString += '<td id="bonusTimes3"></td>';
+      }
+      else if(tr == (board.tilesToASide-1)) {
+        htmlString += '<td id="bonusTimes3"></td>';
+      }
+      else if(td == 0) {
+        htmlString += '<td id="bonusTimes3"></td>';
+      }
+      else if(td == (board.tilesToASide-1)) {
+        htmlString += '<td id="bonusTimes3"></td>';
+      }
+      else if(tr == middle-2 && td == middle -2) {
+       r = tr;
+        while (r < middle+3) {
+          r++;
+          htmlString += '<td id="bonusTimes2"></td>';
+        }
+      }
+      else {
+          htmlString += '<td id="normal"></td>';
+        }
     }
     htmlString += '</tr>';
   }
@@ -18,20 +44,7 @@ App.prototype.render = function() {
 
 
 
-  // How to find an element on the page (using a CSS selector)
   var insertionPoint = $('#insertion-point');
-
-  // How to replace the content of an element with HTML you've created
   insertionPoint.html(htmlString);
   console.log(htmlString);
-  // Your mission, should you choose to accept it, is to render the board you've been given
-  // on the page. You have the building blocks you need:
-  //   - You can create any HTML element by passing it as a string to the dollar function ($)
-  //   - You can find an HTML element that exists on the page, using $('#any.css .selector')
-  //   - You can insert any HTML element in any other HTML element, using someElement.html(foo)
-  // (More information on these and other methods is available at http://api.jquery.com/html)
-  //
-  // Your board must have:
-  //   - The given number of rows and columns (tilesToASide)
-  //   - A visibly different center tile
 };
