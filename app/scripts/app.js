e74f688c4ccceb7360f5dbd38e95a1b4e7ce8bf3
Sophie <a.sophiewirth@gmail.com>
2014-09-02 12:27:36 +0200

    Changed render function and how the board is drawn
    Instead of making the htmlstring directly the tile
    properties are now given into a two dim. array.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,64 +4,30 @@ App = function(board) {
 
 
 App.prototype.render = function() {
-
   var htmlString = '<table>';
-  var middle = ((Math.round(board.tilesToASide/2))-1);
-  var limit = board.tilesToASide;
 
-  var widthOfRing = 7, // 3 on each side plus the 1 tile in the middle
-      minimumBoardSizeForInnerBonusTileRing = widthOfRing + 2, // 1 for space, 1 for the outer ring
-      topOfRing = middle - 3, // These additions/subtractions should be derived from widthOfRing
-      bottomOfRing = middle + 3,
-      leftSideOfRing = middle - 3,
-      rightSideOfRing = middle + 3;
-
-  for(var tr = 0; tr < board.tilesToASide; tr++) {
+  for(var tr = 0; tr < this.board.tilesToASide; tr++) {
     htmlString += '<tr>';
-    for (var td = 0; td < board.tilesToASide; td++) {
-
-      //condition for orange middle tile
-      if((tr == middle)&&(td == middle)){
+    for (var td = 0; td < this.board.tilesToASide; td++) {
+      var tile = this.board.tiles[tr][td];
+      if(tile.middle) {
         htmlString += '<td id="middle"></td>';
       }
-
-
-      //condition for x3 bonus tiles, outer border
-      else if((tr == 0) || (tr == (limit-1)) || (td == 0) || (td == (limit-1))) {
+      else if(tile.multiplier == 3) {
         htmlString += '<td id="bonusTimes3">x3</td>';
       }
-
-      // At the beginning of the top or bottom row of the inner ring, output a number of 2x bonus tiles equal to the
-      // width of the ring.
-      else if( board.tilesToASide >= minimumBoardSizeForInnerBonusTileRing && // Board is large enough to draw the ring
-                tr == topOfRing && td == leftSideOfRing || // We're at the top-left corner of the ring
-                tr == bottomOfRing && td == leftSideOfRing ) // We're at the bottom-left corner of the ring
-      {
-        for(var i = 0; i < widthOfRing; i++) {
-          htmlString += '<td id="bonusTimes2">x2</td>';
-        }
-        td = rightSideOfRing;
-      }
-
-      // condition for x2 bonus tiles, inner border, left and right columns
-      else if(/* left side */(td == (middle-3) || /* right side */td == (middle+3)) &&
-        /* condition, so the tiles don't fill out the entire column*/
-        (!(tr<=(middle-3)) && !(tr>=(middle+3)))) {
+      else if(tile.multiplier == 2) {
         htmlString += '<td id="bonusTimes2">x2</td>';
       }
-
       else {
         htmlString += '<td id="normal"></td>';
       }
-
     }
     htmlString += '</tr>';
   }
   htmlString += '</table>';
 
-
-
   var insertionPoint = $('#insertion-point');
   insertionPoint.html(htmlString);
-  console.log(" " + htmlString + " ");
+//  console.log(" " + htmlString + " ");
 };
--- app/scripts/board.js
+++ app/scripts/board.js
@@ -17,3 +17,47 @@ Board.prototype.placeToken = function(row, col, token) {
   if(this.tiles[row][col].token) throw 'full!!!';
   this.tiles[row][col].token = token;
 };
+
+Board.prototype.setRingPattern = function() {
+  var middle = ((Math.round(this.tilesToASide/2))-1),
+      limit = this.tiles.length,
+      widthOfRing = 7, // 3 on each side plus the 1 tile in the middle
+      minimumBoardSizeForInnerBonusTileRing = widthOfRing + 2, // 1 for space, 1 for the outer ring
+      topOfRing = middle - 3, // These additions/subtractions should be derived from widthOfRing
+      bottomOfRing = middle + 3,
+      leftSideOfRing = middle - 3,
+      rightSideOfRing = middle + 3;
+
+  for(var tr = 0; tr < this.tiles.length; tr++) {
+    for (var td = 0; td < this.tiles.length; td++) {
+      //condition for orange middle tile
+      if((tr == middle)&&(td == middle)){
+        this.tiles[tr][td].middle = true;
+      }
+
+      //condition for x3 bonus tiles, outer border
+      else if((tr == 0) || (tr == (limit-1)) || (td == 0) || (td == (limit-1))) {
+        this.tiles[tr][td].multiplier = 3;
+      }
+
+      // At the beginning of the top or bottom row of the inner ring, output a number of 2x bonus tiles equal to the
+      // width of the ring.
+      else if( this.tilesToASide >= minimumBoardSizeForInnerBonusTileRing && // Board is large enough to draw the ring
+                tr == topOfRing && td == leftSideOfRing || // We're at the top-left corner of the ring
+                tr == bottomOfRing && td == leftSideOfRing ) // We're at the bottom-left corner of the ring
+      {
+        for(var i = 0; i < widthOfRing; i++) {
+          this.tiles[tr][td + i].multiplier = 2;
+        }
+        td = rightSideOfRing;
+      }
+
+      // condition for x2 bonus tiles, inner border, left and right columns
+      else if(/* left side */(td == (middle-3) || /* right side */td == (middle+3)) &&
+        /* condition, so the tiles don't fill out the entire column*/
+        (!(tr<=(middle-3)) && !(tr>=(middle+3)))) {
+        this.tiles[tr][td].multiplier = 2;
+      }
+    }
+  }
+};
